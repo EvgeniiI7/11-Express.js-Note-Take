be96f307ce15695ae88cd1fce3882ed2b2d87aa0
Evgenii Ivanov <evgeniiivanov@Evgeniis-MacBook-Pro.local>
2021-12-14 00:36:10 -0500

    initial commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require("express");
 
 let app = express();
 
-let PORT_URL = process.env.PORT || 8000;
+let PORT_URL = process.env.PORT || 3000;
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -14,7 +14,7 @@ app.use(express.static("./public"));
 ////pull routing from this js file 
 require("./public/assets/js/routing")(app);
 
-////set up local host to run on port 8080 and tell me its connected
+////set up local host to run on port 3000 and tell me its connected
 app.listen(PORT_URL, function() {
     console.log("listening on port " + PORT_URL);
 });
